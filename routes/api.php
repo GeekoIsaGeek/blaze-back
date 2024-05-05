6e48221da1696552b7068d537eca6c440e31a116
geekoisageek <george.kvrivi@gmail.com>
2024-05-05 23:09:13 +0400

    chore: remove unneeded api resource

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\UserController;
 use App\Http\Resources\InterestResource;
 use App\Models\Interest;
-use App\Models\User;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user()->load('preference', 'photos', 'interests')->append('age');
